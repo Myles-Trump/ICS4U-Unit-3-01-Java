3a099b3867604dc5b76ff64a6ae86ef63378e851
Ubuntu <ubuntu@ip-172-31-84-211.ec2.internal>
2021-12-23 00:46:18 +0000

    Saved code

--- Lock.java
+++ Lock.java
@@ -1,10 +1,22 @@
+/*
+* The "Lock" Program aids Main.java
+*
+* @author  Myles Trump
+* @version 1.0
+* @since   2021-12-22
+*/
 public class Lock {
 
     // the Lock class has 1 field
-    int combination = 0000;
+    private int combination = 0000;
 
     // and 1 method
-    public void changeCombination(int inputtedChange) {
+    /**
+    * This method changes the combination.
+    *
+    * @param inputtedChange the new combination the user inputted
+    */
+    public void changeCombination(final int inputtedChange) {
         combination = inputtedChange;
     }
 }
